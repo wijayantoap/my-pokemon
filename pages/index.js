f15cda4daa878733ff12f5e1038e7aebbcaa7029
Wijayanto Abdurrahman Prawirodirjo <wijayanto.a.p@gmail.com>
2021-09-27 15:43:53 +0700

    Add apollo client

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,61 @@
-import styles from '../styles/Home.module.css'
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://graphql-pokeapi.graphcdn.app",
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <div>
+      <PokemonList />
+    </div>
+  );
+}
+
+function PokemonList() {
+  const POKEMON_LIST = gql`
+    query pokemons($limit: Int, $offset: Int) {
+      pokemons(limit: $limit, offset: $offset) {
+        count
+        next
+        previous
+        nextOffset
+        prevOffset
+        status
+        message
+        results {
+          url
+          name
+          image
+        }
+      }
+    }
+  `;
+
+  const { loading, error, data } = useQuery(POKEMON_LIST);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
+  return data.pokemons.results.map((pokemon, index) => (
+    <div key={index}>
+      <p>{pokemon.name}</p>
+    </div>
+  ));
+}
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
 }
