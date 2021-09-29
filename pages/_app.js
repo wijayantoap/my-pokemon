6280e5f9ab5332fccabe15a2ec5e42a2edf41e99
Wijayanto Abdurrahman Prawirodirjo <wijayanto.a.p@gmail.com>
2021-09-29 13:12:51 +0700

    Add progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,13 @@ import { ApolloProvider } from "@apollo/client";
 import Head from "next/head";
 import client from "../configs/apollo-client";
 import "../styles/globals.css";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
